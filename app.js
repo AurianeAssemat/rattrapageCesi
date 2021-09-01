01742063b8682e892c8a8d4b5969ff491d2bc8e2
AurianeAssemat <43166512+AurianeAssemat@users.noreply.github.com>
2021-09-01 19:35:48 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,18 +7,29 @@ const server = http.createServer(app);
 var port = 8282;
 
 server.listen(port, function(){
-	logger.log('info', "Server listenning on "+port+" !");
+	console.log("Server listenning on "+port+" !");
 });
 
 app.get('/', function(req, res){
-	filename = "./accueil.html";
-	fs.readFile(filename, function(err, data) {
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Page Not Found");
-		}
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		res.end();
-	});
+	var txt = '<!DOCTYPE HTML><html><head><meta charset="utf8" /><title>App Node Js</title></head><body> ' +
+			'<style>h1 {color: #00A0DD; text-align: center; font-family:Verdana; } p {text-align:center; font-family:Calibri; } '+
+			'a.href {font-family:Calibri; } a:link {color:blue;} a:visited{color:blue;}</style></style><h1>Accueil</h1><br><br><p>Bienvenue</p>' +
+			'<br><br><p><form id="messageCesi" name="messageCesi"  action="/echo" method="GET" >' +
+			'Message : <input id="message" name="message" type="text" maxlength="250"><br>'+
+			'<input type="submit" value="Valider"></form></p></body></html>'
+	res.writeHead(200, {"Content-Type": "text/html"})
+	res.write(txt)
+	res.end()
+});
+
+
+app.get('/echo', function(req, res){
+	var message = url.parse(req.url, true).query.message;
+	var txt ='<!DOCTYPE HTML><html><head><meta charset="utf8" /><title>Rattrapage</title></head>'+
+		'<body><style>h1 {color: #00A0DD; text-align: center; font-family:Verdana; } p {text-align:center; font-family:Calibri; } '+
+		'a.href {font-family:Calibri; } a:link {color:blue;} a:visited{color:blue;}</style>'+
+		'<h1>Echo !</h1><br><p>'+message+'</p><br><br><p><a href="/">Accueil</a></p></body></html>'
+	res.writeHead(200, {"Content-Type": "text/html"})
+	res.write(txt)
+	res.end()
 });
